2f27767f7dabdaaa63c33a0d3be0be7bf5714361
wnstn819 <wnstn819@naver.com>
2023-02-17 20:59:57 +0900

    피드백 페이지 댓글 부분 구현

--- feedBack/app.py
+++ feedBack/app.py
@@ -1,12 +1,15 @@
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
 from flask import Flask, render_template,jsonify,request
+from datetime import datetime
 app = Flask(__name__)
 
-#client = MongoClient('mongodb://test:test@localhost', 27017)
-client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost', 27017)
+#client = MongoClient('localhost', 27017)
 db = client.kraft
-
+db.commentList.drop()
+db.counters.drop()
+db.counters.insert_one({"seq" : 0})
 
 @app.route('/')
 def home():
@@ -14,7 +17,41 @@ def home():
 
 @app.route('/feedback/list', methods=['GET'])
 def card_list():
+    result = list(db.commentList.find({}))
+    return jsonify({'result': 'success', 'list' : result})
+
+
+@app.route('/feedback/list/add', methods=['POST'])
+def card_add():
+    name = request.form['name']
+    pw = request.form['pw']
+    contents = request.form['contents']
+    date = datetime.today().strftime("%Y-%m-%d")
+
+    seq = db.counters.find({})[0]['seq']
+    doc = {
+        '_id' : seq,
+        'name' : name,
+        'pw' : pw,
+        'contents' : contents,
+        'date' : date,
+    
+    }
+    db.commentList.insert_one(doc)
+    db.counters.update_one({'seq': seq},{'$set':{'seq' : (seq+1)}})
+
+    
+    return jsonify({'result': 'success'})
+
+
+@app.route('/feedback/list/delete', methods=['POST'])
+def card_delete():
+
+    id = request.form['id']
+    db.commentList.delete_one({'_id' : int(id)})
    
+
+    
     return jsonify({'result': 'success'})
 
 
