1e0238d6a8d7d9eee9bbbdfa5f586ab9d9cf7676
wnstn819 <wnstn819@naver.com>
2023-02-18 13:20:16 +0900

    메모장 2.0

--- memoSeq/app.py
+++ memoSeq/app.py
@@ -1,15 +1,15 @@
 from pymongo import MongoClient
-from bs4 import BeautifulSoup
-from bson.objectid import ObjectId
 from flask import Flask, render_template,jsonify,request
 app = Flask(__name__)
 
-client = MongoClient('mongodb://test:test@localhost', 27017)
+client = MongoClient('mongodb://test:test@54.180.97.170', 27017)
 #client = MongoClient('localhost', 27017)
-db = client.kraft
-db.counters.drop()
-db.memoList.drop()
-db.counters.insert_one({"seq" : 0})
+db = client.dbjungle
+
+if 'counters' in db.list_collection_names():
+    print(db.list_collection_names())
+else :
+    db.counters.insert_one({"seq" : 0})
 
 @app.route('/')
 def home():
@@ -17,7 +17,7 @@ def home():
 
 @app.route('/memo/list', methods=['GET'])
 def card_list():
-    result = list(db.memoList.find({}))
+    result = list(db.memos.find({}).sort('like', -1))
     return jsonify({'result': 'success', 'list': result})
     
 
@@ -31,9 +31,10 @@ def add_card():
         '_id' : seq,
         'title' : title,
         'text' : text,
+        'like' : 0,
     
     }
-    db.memoList.insert_one(doc)
+    db.memos.insert_one(doc)
     db.counters.update_one({'seq': seq},{'$set':{'seq' : (seq+1)}})
     
 
@@ -45,7 +46,7 @@ def update_list():
     _id = request.form['id']
     title = request.form['title']
     text = request.form['text']
-    db.memoList.update_one({'_id' :int(_id)} ,{'$set':{ 'title': title, 'text' : text}})
+    db.memos.update_one({'_id' :int(_id)} ,{'$set':{ 'title': title, 'text' : text}})
 
     return jsonify({'result': 'success'})
 
@@ -54,7 +55,17 @@ def update_list():
 def delete_list():
 
     _id = request.form['id']
-    db.memoList.delete_one({'_id' : int(_id)})
+    db.memos.delete_one({'_id' : int(_id)})
+
+    return jsonify({'result': 'success'})
+
+@app.route('/memo/like', methods=['POST'])
+def like_list():
+
+    _id = request.form['id']
+    like = db.memos.find_one({'_id' : int(_id)})['like']
+    print(like)
+    db.memos.update_one({'_id': int(_id)}, {'$set' : {'like': like+1}})
 
     return jsonify({'result': 'success'})
 
